4e89d79b90618b76c8a6cbd3838a85d6ef8db323
Michael Kozub <michaelkozub@gmail.com>
2019-05-03 16:41:10 -0600

    adding comments

--- Sorting.playground/Contents.swift
+++ Sorting.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 
+//sorts evens on left, odds on right
 let output = [1, 4, 2, 3, 7, 0, 9].sorted { (lhs, rhs) -> Bool in
     let lhsIsEven = lhs.isMultiple(of: 2)
     let rhsIsEven = rhs.isMultiple(of: 2)
@@ -17,18 +18,16 @@ let output = [1, 4, 2, 3, 7, 0, 9].sorted { (lhs, rhs) -> Bool in
 
 print(output)
 
-let output2 = [1, 4, 2, 3, 7, 0, 9].sorted { (lhs, rhs) -> Bool in
-    return lhs > rhs
-}
+let output2 = [1, 4, 2, 3, 7, 0, 9].sorted() //default implementation sorts ascending
 
 print(output2)
 
 let output3 = ["Mike", "Andy", "Zack", "Aurora", "Duncan"].sorted { (lhs, rhs) -> Bool in
-    return lhs < rhs
+    return lhs < rhs //sorts alphabetically ascending
 }
 
 let output4 = ["Mike", "Andy", "Zack", "Aurora", "Duncan"].sorted { (lhs, rhs) -> Bool in
-    return lhs > rhs
+    return lhs > rhs //sorts alphabetically descending
 }
 
 print(output3)
